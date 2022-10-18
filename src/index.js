1dd2c8c0a62240bd126c45f67e7a5bbad811eb42
Lindsay Taylor <llindsayttaylor@gmail.com>
2022-10-18 11:34:12 -0400

    set up GET fetch and created divs for toys from JSON

--- src/index.js
+++ src/index.js
@@ -12,4 +12,57 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  //FETCHING JSON DATA
+  fetch("http://localhost:3000/toys")
+  .then( response => response.json())
+  .then( collection => {
+    collection.forEach(toy => {
+      newToyCard(toy);
+    })
+  });
+
+
+  //POST REQUEST DATA TO JSON
+
+
+
+
+  fetch("http://localhost:3000/toys", { configurationObject} )
+  .then (response => response.json())
+  .then (object => {
+    console.log(object);
+  })
+
+  //ADD TOY INFO TO CARD
+  function newToyCard (toy){
+    const divContainer = document.querySelector("#toy-collection");
+
+    const div = document.createElement("div");
+    div.classList.add("card");
+    
+    const header = document.createElement("h2");
+    header.textContent = toy.name;
+
+    const img = document.createElement("img");
+    img.classList.add("toy-avatar");
+    img.src = toy.image;
+
+    const p = document.createElement("p");
+    const likes = toy.likes
+    p.textContent = `${likes} Likes`
+
+    const button = document.createElement("button");
+    button.classList.add("like-btn");
+    button.setAttribute(`id`, `${toy.id}`);
+    button.textContent = "Like ❤️";
+
+    divContainer.append(div);
+    div.append(header, img, p, button);
+
+  };
+
+  //ADD A NEW TOY
+
+
 });
